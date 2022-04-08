2bed0b41294fd0976c477988dcb1a960502f26cc
symplymarve <adegboromayowa@yahoo.com>
2022-04-08 06:52:28 -0700

    4-print_alphabt.c-task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
 * main - main block
-* description: get a random number and check its last digit, compare it with 5
-* return: 0
+* Description: get a random number and check its last digit, compare it with 5
+* Return: 0
 */
 int main(void)
 {
 char c = 'a';
-while (c ,= 'z')
+while (c <= 'z')
 {
 if (c != 'e' && c != 'q')
 {
