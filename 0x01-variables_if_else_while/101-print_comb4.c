16fbbb39c8baf851e4f70ddf30030fefcafb5480
symplymarve <adegboromayowa@yahoo.com>
2022-04-08 13:58:05 -0700

    101-print_comb4.c-adv task 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,7 @@
 * Number should be printed in acsending oder, with two digits
 * You can only use the putchar function
 * (every other function (printf, puts, etc) is forbidden)
-* you can only use putchar five times maximum in your code
+* You can only use putchar five times maximum in your code
 * You are not allowed to use any variable of type char
 * All your code should be in the main function 
 * Return: 0
@@ -21,9 +21,6 @@ int main(void)
 int c;
 int d;
 int e = 0;
-while (e < 10)
-{
-d = 0
 while (e < 10)
 {
 d = 0;
