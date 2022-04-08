367ee6f3a4112a43b37df2e3bbb469069d85148f
symplymarve <adegboromayowa@yahoo.com>
2022-04-08 07:22:28 -0700

    6-print_numberz.c-task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - main block
-* Description: prints all single digit numbers of base 20
+* Description: prints all single digit numbers of base 10
 * starting from 0, followed by a new line.
 * Return: 0
 */
