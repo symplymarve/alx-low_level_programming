2fadd24437719858a596dd7d3d67123b3ee9cd90
symplymarve <adegboromayowa@yahoo.com>
2022-04-08 06:39:37 -0700

    3-print_alphabets.c-task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,14 +3,14 @@
 * main - main block
 * Description: prints the alphabet in lowercase,
 * and then in uppercase, followed by a new line.
-*Return: 0
+* Return: 0
 */
 int main(void)
 {
 char c = 'a';
 while (c <= 'z')
 {
-putcha(c);
+putchar(c);
 c++;
 }
 c = 'A';
