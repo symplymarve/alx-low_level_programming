35b1e7c4729b52c769a63f8d1646c128be60cf3c
symplymarve <adegboromayowa@yahoo.com>
2022-04-12 11:32:55 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,17 @@
-#ifndef MAIN_H_INCLUDED
+#ifndef MAIN_H_INCLUDED 
 #define MAIN_H_INCLUDED
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isaplha(int c);
 int print_sign(int n);
-int_abs(int n);
+int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_tables(int n);
-#end if
+#endif
 
 
