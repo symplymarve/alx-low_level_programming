c5f48cbb796526199f6d3214385477f22632b168
symplymarve <adegboromayowa@yahoo.com>
2022-04-12 16:46:53 -0700

    adv task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,11 +9,11 @@
 void print_times_table(int n)
 {
 int row, column, product;
-if (n >=0 && n< 15)
+if (n >= 0 && n < 15)
 {
-for (row = 0; row <=n; row++)
+for (row = 0; row <= n; row++)
 {
-for (column = 0; column <=n; column++)
+for (column = 0; column <= n; column++)
 {
 product = row * column;
 if (column == 0)
