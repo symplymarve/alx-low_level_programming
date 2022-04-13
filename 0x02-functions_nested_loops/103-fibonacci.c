64bd9e2fe4173416f844e24295d126ddb1ea5b08
symplymarve <adegboromayowa@yahoo.com>
2022-04-13 02:02:58 -0700

    adv task 4

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,23 +4,23 @@
 * followed by a new line
 * Return: Always 0 (Success)
 */
-int main(void) 
-{ 
+int main(void)
+{
 int i;
-unsigned long int j, k, next, sum; 
+unsigned long int j, k, next, sum;
 j = 1;
-k = 2; 
-sum = 0; 
-for (i = 1; i <= 33; ++i)   
+k = 2;
+sum = 0;
+for (i = 1; i <= 33; ++i)
 {      
 if (j < 4000000 && (j % 2) == 0)
-{  
+{
 sum = sum + j;
 }     
-next = j + k;    
-j = k;    
+next = j + k;
+j = k;
 k = next;
 }
 printf("%lu\n", sum);
-return (0); 
+return (0);
 }
