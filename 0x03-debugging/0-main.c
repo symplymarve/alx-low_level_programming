49cb5f927b9c535bfc156b56e5330a978fef41e9
symplymarve <101419536+symplymarve@users.noreply.github.com>
2022-04-14 12:58:19 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
 /**
- * main - test function if integer is positive or negative
+ * main - tests function if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
-int i;
-  
+int i; 
 i = 0;
-positive_or_negative(i);
-  
+positive_or_negative(i);  
 return (0)
 }
