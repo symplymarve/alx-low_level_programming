d2a8b3f52c600c0af0c74714d6e527d412ed9ffd
symplymarve <adegboromayowa@yahoo.com>
2022-04-14 08:47:15 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h
+#include "main.h"
 
 /**
 * main - tests function if integer is positive or negative
@@ -7,7 +7,6 @@
 int main(void)
 {
 int i;
-
 i = 0;
 positive_or_negative(i);
 
