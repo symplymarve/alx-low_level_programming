6aa6ba135bcb66b6629b7749465aa8d1d99bcfcc
Howdy-admoll <admoll.adefemi@gmail.com>
2022-04-14 20:45:07 +0100

    project task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashtags to make a square
+ * @size: limit
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
