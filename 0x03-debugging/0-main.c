5e815380d099b7827a2b489b98706d9077e2de2a
symplymarve <adegboromayowa@yahoo.com>
2022-04-14 04:25:30 -0700

    multiple mains-task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
-* main - tests function if integer positive or negative
-* Return: 0
-*/
+ * main - tests if integer is positive or negative
+ * Return: 0
+ */
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
 return (0);
