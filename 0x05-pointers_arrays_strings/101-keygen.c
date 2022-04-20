07c1ee6583cc1a6b8cddd86d058c10da93043806
symplymarve <adegboromayowa@yahoo.com>
2022-04-20 22:45:04 +0100

    updated

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,22 +3,21 @@
 #include <time.h>
 
 /**
-* main - generates random passwords for 101-crackme
-* Return: zero
-*/
-
+  * main - generates random passwords for 101-crackme
+  * Return: zero
+  */
 int main(void)
 {
-int sum;
-char c;
+	int sum;
+	char c;
 
-srand(time(NULL));
-while (sum <= 2645)
-{
-c = rand() % 128;
-sum += c;
-putchar(c);
-}
-putchar(2772 - sum);
-return (0);
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }
