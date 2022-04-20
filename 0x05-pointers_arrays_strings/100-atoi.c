d4860a7929d9e0c736591bbda43a1277e44823cf
symplymarve <adegboromayowa@yahoo.com>
2022-04-20 13:25:09 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,12 @@
 int _atoi(char *s)
 {
 unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-while (*(s + coun != '\0')
+while (*(s + count != '\0')
 if (size > 0 && (*(s + count) <'0' || *(s + count) > '9'))
 break;
 if (*(s + count) == '-')
 pn *= -1;
-if ((*(s + count) >= '0') && (*(s + count) > '9'))
+if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 {
 if (size > 0)
 m *= 10;
