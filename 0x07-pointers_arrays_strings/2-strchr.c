ecf65333b93eea13daba8ac290ccf0a53d4c0ade
symplymarve <adegboromayowa@yahoo.com>
2022-04-26 11:41:48 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* _strchr - function that gets the length of a prefix substring
+* _strchr - function that locates a character in a string
 * @s: the string to check
 * @c: the character we're looking for
 * Return: a pointer to the first occurance of the character @c in the string
