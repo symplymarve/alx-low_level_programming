aacf2079c59347130713182b2bba926916b200bb
symplymarve <adegboromayowa@yahoo.com>
2022-04-26 11:05:42 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,9 @@
 
 /**
 * _memcpy - Copies memory area
-*
 * @dest: where to copy the memory to
 * @n: the number of bytes
 * @src: the memory area to copy from
-*
 * Return: a pointer to @dest
 */
 
