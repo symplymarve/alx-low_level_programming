b6c0e8649910484adc45da11aa679a0d8bc1723f
symplymarve <adegboromayowa@yahoo.com>
2022-04-28 10:49:15 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,11 @@
  * Return: the factorial of @n
  */
 
-int factorial(int n) 
+int factorial(int n)
 {
 if (n < 0)
 return (-1);
 else if (n == 0)
 return (1);
-return (n * factorial(n - 1)); 
+return (n * factorial(n - 1));
 }
