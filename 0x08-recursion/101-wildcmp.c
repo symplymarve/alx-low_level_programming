ddde6102d57782c4b18ea77ecea5700b296a2780
symplymarve <adegboromayowa@yahoo.com>
2022-04-28 12:55:10 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,7 +6,7 @@
  * @s2: string 2. It can contains a * as a special character.
  * Return: 1 if are identical, 0 if not.
  */
-int wildcmp(char *s1, char *s2) 
+int wildcmp(char *s1, char *s2)
 {
 if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 return (0);
@@ -16,5 +16,5 @@ if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
 return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-return (0); 
+return (0);
 }
