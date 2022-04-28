e501087ecba0191132cf04bdc1a1e37f158d307c
symplymarve <adegboromayowa@yahoo.com>
2022-04-28 11:03:59 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,11 +6,11 @@
  * @y: the power to raise @n to
  * Return: the value of @x to the @y power
  */
-int _pow_recursion(int x, int y) 
+int _pow_recursion(int x, int y)
 {
 if (y < 0)
 return (-1);
 if (y == 0)
 return (1);
-return (x * (_pow_recursion(x, y - 1)));  
+return (x * (_pow_recursion(x, y - 1)));
 }
