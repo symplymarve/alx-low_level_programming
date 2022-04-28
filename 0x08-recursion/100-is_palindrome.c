7edd7b1f318b96bd89af76c2bdca4b5d9fb83b48
symplymarve <adegboromayowa@yahoo.com>
2022-04-28 12:46:41 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,7 +5,7 @@
 * @s: string
 * Return: the length of a string.
 */
-int _strlen_recursion(char *s) 
+int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
@@ -19,14 +19,14 @@ return (1 + _strlen_recursion(s + 1));
  * @n2: biggest iterator.
  * Return: .
  */
-int comparator(char *s, int n1, int n2) 
+int comparator(char *s, int n1, int n2)
 {
 if (*(s + n1) == *(s + n2))
 {
 if (n1 == n2 || n1 == n2 + 1)
 return (1);
 return (0 + comparator(s, n1 + 1, n2 - 1));
-} 
+}
 return (0);
 }
 /**
@@ -35,7 +35,7 @@ return (0);
  * Return: 1 if s is a palindrome, 0 if it's not
  */
 int is_palindrome(char *s)
-{  
+{
 if (*s == '\0')
 return (1);
 return (comparator(s, 0, _strlen_recursion(s) - 1));
