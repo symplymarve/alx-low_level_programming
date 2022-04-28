685907bdd971253920a99254548cdd2d0ab4fe2b
symplymarve <adegboromayowa@yahoo.com>
2022-04-28 12:26:46 -0700

    task 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,40 +1,41 @@
 #include "main.h"
 
 /**
- * palindromeChecker - checks the string
- * @str: the string to be checked
- * @len: the length of the string
- * @i: the incrementor, starts at 0
- * Return: 1 if it's a palindrome, 0 if it's not
- */
-int palindromeChecker(char *str, int len, int i) 
-{
-if (i < len && str[i] == str[len])
-return (palindromeChecker(str, len - 1, i + 1));
-if (str[i] != str[len])
-return (0);
-return (1); 
-}
-/**
- * _strlen_recursion - returns the length of a string
- * @s: the string to check the length of
- * Return: an integer that displays the length of the string
- */
+* _strlen_recursion - returns the length of a string.
+* @s: string
+* Return: the length of a string.
+*/
 int _strlen_recursion(char *s) 
 {
-if (*s != '\0')
-return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
 return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
 /**
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
+ */
+int comparator(char *s, int n1, int n2) 
+{if (*(s + n1) == *(s + n2))
+{
+if (n1 == n2 || n1 == n2 + 1)
+return (1);
+return (0 + comparator(s, n1 + 1, n2 - 1));
+} 
+return (0);
+ /**
  * is_palindrome - checks to see if a string is a palindrome
- *
  * @s: the string to check
- *
- * Return: 1 if it's a palindrome, 2 if it's not
+ * Return: 1 if s a palindrome, 0 if it's not
  */
 int is_palindrome(char *s)
-int i = 0;
-int length = _strlen_recursion(s) - 1;
-return (palindromeChecker(s, length, i)); 
+{  
+if (*s == '\0')
+return (1);
+return (comparator(s, 0, _strlen_recursion(s) - 1));
+}
 }
