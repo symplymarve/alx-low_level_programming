c71701529d3cfec2defb35d78b77fd3cbca60514
symplymarve <adegboromayowa@yahoo.com>
2022-04-28 10:36:19 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,9 @@
 * Return: an integer that displays the length of the string
 */
 
-int _strlen_recursion(char *s) 
+int _strlen_recursion(char *s)
 {
 if (*s != '\0')
 return (1 + _strlen_recursion(s + 1));
-return (0);  
+return (0);
 }
