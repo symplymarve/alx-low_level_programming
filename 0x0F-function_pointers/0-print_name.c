05e2fa111de6fb2fd2522f5141d314ee21f3cf87
symplymarve <adegboromayowa@yahoo.com>
2022-05-13 05:08:31 -0700

    task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,14 +2,12 @@
 
 /**
  * print_name - prints a name
- * @name: the name to print
- * @f: the function to send the name to
- * Return: nothing
+ * @name: char string
+ * @f: function pointer that takes a string argument
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 if (!name || !f)
-return;
 f(name);
 }
