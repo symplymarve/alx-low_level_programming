4c9c4139601f20a4011ed608bb0d379e73262341
symplymarve <101419536+symplymarve@users.noreply.github.com>
2022-05-13 13:39:03 +0100

    updated task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,19 +4,12 @@
  * print_name - prints a name
  * @name: char string
  * @f: function pointer that takes a string argument
- * Return: Nothing.
 */
 
-void print_name(char *name, void (*f)(char *))
-  void print_name(char *name, void (*f)(char *))
-  
+void print_name(char *name, void (*f)(char *))  
 {
-  
-  if (name && f)
-    
-    {
-      
-      f(name);
-      
-    }
-    }
+if (name && f)
+{
+f(name);
+}
+}
