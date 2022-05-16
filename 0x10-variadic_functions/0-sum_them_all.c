4e20cdfc1433178f0bb0dd2aa6df107c57e4cd0c
symplymarve <101419536+symplymarve@users.noreply.github.com>
2022-05-16 18:32:09 +0100

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,13 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-
 /**
- * sum_them_all - returns the sum of all parameters
+ * sum_them_all - returns the sum of all its parameters
  * @n: the numbers of arguments being passed through
  * Return: If n == 0, return 0
  * otherwise - the sum of all parameters
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 va_list valist;
