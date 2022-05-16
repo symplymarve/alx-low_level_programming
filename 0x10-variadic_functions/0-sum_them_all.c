22686ef6a88f0c3dd78f76c84969f69b5aefa4d7
symplymarve <101419536+symplymarve@users.noreply.github.com>
2022-05-16 18:13:07 +0100

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,10 +4,10 @@
 /**
  * sum_them_all - returns the sum of all parameters
  * @n: the numbers of arguments being passed through
- * ...: variable number of paramters to calculate the sum of
  * Return: If n == 0, return 0
- * otherwise the sum of all parameters
+ * otherwise - the sum of all parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 va_list valist;
@@ -15,7 +15,7 @@ unsigned int sum = 0, i;
 if (n == 0)
 return (0);
 va_start(valist, n);
-for (i = 0; i < n; i++) 
+for (i = 0; i < n; i++)
 sum += va_arg(valist, int);
 va_end(valist);
 return (sum);
